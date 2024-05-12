8aa23748b16a5f02aff11815fb8123cf5cda7e54
Guillermo Mendez <l20121017@morelia.tecnm.mx>
2024-05-11 20:48:14 -0600

    FEAT Implementacion de Frame de input para su futura implementacion con Backend

--- entornoVirtual/OpenAI/Clasificacion_OpenAI.py
+++ entornoVirtual/OpenAI/Clasificacion_OpenAI.py
@@ -4,39 +4,40 @@ from numpy.linalg import norm
 from openAIapi import obtain_embedding
 from similitud import similitud_cos
 
-dirBD = "C:/Users/Link_/Desktop/LA_II/Proyecto/COELNETSSO/entornoVirtual/BD/bd_prototipo.json"
+def clasificationOpenAI():
+    dirBD = "C:/Users/Link_/Desktop/LA_II/Proyecto/COELNETSSO/entornoVirtual/BD/bd_prototipo.json"
+    texto = input("Ingresa Cadena a Clasificar: ")
 
-texto = input("Ingresa Cadena a Clasificar: ")
+    #Obtiene vector del texto
+    vecInput = obtain_embedding(texto)
 
-#Obtiene vector del texto
-vecInput = obtain_embedding(texto)
+    #Carga de Json con Clasificacion de Ordenes
+    with open(dirBD, 'r') as archivo:
+        datos = json.load(archivo)
 
-#Carga de Json con Clasificacion de Ordenes
-with open(dirBD, 'r') as archivo:
-    datos = json.load(archivo)
+    similitudes = []
 
-similitudes = []
+    #imprimir similitudes promedio por categoria
+    for clave, valor in datos.items():
+        i = 0
+        sum = 0.0
+        
+        for elemento in valor:
+            vecBD=obtain_embedding(elemento)
+            sim=similitud_cos(vecInput,vecBD)
+            sum=sum+sim
+            i=i+1
 
-#imprimir similitudes promedio por categoria
-for clave, valor in datos.items():
-    i = 0
-    sum = 0.0
-    
-    for elemento in valor:
-        vecBD=obtain_embedding(elemento)
-        sim=similitud_cos(vecInput,vecBD)
-        sum=sum+sim
+        similitudes.append(sum/i)
+        print(clave + ": "+str(round(sum/i,3)))
+    print()
+
+    max_similitud = similitudes.index(max(similitudes))
+
+    i=0
+    for clave, valor in datos.items():
+        if i == max_similitud:
+            print("El texto tiene mayor similitud semantica con la Categoría: "+ clave)
+            break
         i=i+1
-
-    similitudes.append(sum/i)
-    print(clave + ": "+str(round(sum/i,3)))
-print()
-
-max_similitud = similitudes.index(max(similitudes))
-
-i=0
-for clave, valor in datos.items():
-    if i == max_similitud:
-        print("El texto tiene mayor similitud semantica con la Categoría: "+ clave)
-        break
-    i=i+1
+    return similitudes, max_similitud
---
+++ entornoVirtual/app.py
@@ -0,0 +1,100 @@
+from fastapi import FastAPI
+from reactpy import component, html, use_state
+from reactpy.backend.fastapi import configure
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="entornoVirtual/static"), name="static")
+
+@component
+def App():
+    input_value, set_input_value = use_state("")
+
+    def handle_change(event):
+        new_value = event['target']['value']
+        set_input_value(new_value)
+
+    def handle_click(event):
+        print(input_value)
+
+    return html.div(
+        {"style": {"text-align": "left", "margin-top": "20px"}},
+        html.style("""
+            body {
+                font-family: Arial, sans-serif; /* Cambia el tipo de letra */
+            }
+            .background {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-image: url('/static/ya.jpg'); 
+                background-size: cover; 
+                z-index: -1; /* Asegura que el fondo esté detrás de los otros elementos */
+                opacity: 0.9; /* Ajusta la opacidad del fondo */
+            }
+            h1 {
+                text-align: center; /* Centra el texto dentro del h1 */
+            }
+            h1, h2, button {
+                color: white; /* Cambia el color del texto a blanco */
+            }
+            input[type="text"] {
+                width: 50%; 
+                height: 50px;
+                margin-bottom: 10px; /* Añade espacio debajo del input */
+                border-radius: 10px; /* Redondear los bordes del input */
+            }
+            button {
+                width: 50%; /* Hacer el botón del mismo tamaño que el input */
+                display: block; /* Hacer que el botón ocupe toda la anchura disponible */
+                background-color: darkblue; /* Cambiar el color de fondo del botón */
+                height: 40px; /* Ajustar la altura del botón */
+                border-radius: 10px; /* Redondea los bordes del botón */
+                color: white; /* Cambia el color del texto del botón a blanco */
+                border: none; /* Quita el borde del botón */
+                margin-bottom: 30px; /* Añade espacio debajo del botón */
+            }
+            p {
+                width: 50%; /* Establece el mismo ancho que el input */
+                border: 1px solid black; /* Agrega un borde negro */
+                border-radius: 10px; /* Redondea los bordes del párrafo */
+                padding: 10px; /* Añade un espacio alrededor del contenido del párrafo */
+                box-sizing: border-box; /* Incluye el padding en el tamaño total del párrafo */
+                background-color: white;
+                margin-bottom: 10px; /* Añade espacio debajo del párrafo */
+            }
+            h2 {
+                width: 50%; /* Establece el mismo ancho que el input */
+                text-align: left; /* Alinea el texto a la izquierda */
+                margin-bottom: 20px; /* Añade espacio debajo del h2 */
+            }
+        """),
+        html.div({"class": "background"}),  # Agrega un div para el fondo
+        html.h1("Mi aplicación"),  # Estilo centrado
+        html.h2("Instrucción:"),
+        html.input(
+            {"type": "text",
+             "placeholder": "Escribe aquí...",
+             "value": input_value,
+             "onChange": lambda event: handle_change(event),
+            }
+        ),
+        html.button(
+            {"onClick": lambda event: handle_click(event)},
+            "Mandar Instrucción"
+        ),
+        html.h2("Porcentaje de similitud con cada categoría:"),  # Agregar salida de texto
+        
+        html.p("Comida: "  ),
+        html.p("Música: "  ),
+        html.p("Dispositivos Inteligentes: "  ),
+        html.p("Compras: "  ),
+        html.p("Recordatorios: "  ),
+        
+        html.h2("Resultado:"),
+        html.p("La instrucción tiene mayos similitud semántica con la categoria: " + input_value),
+    )
+
+configure(app, App)
